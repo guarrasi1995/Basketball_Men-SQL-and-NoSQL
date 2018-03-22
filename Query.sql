ff76bd9bf0360fd0e5e7a699982503ce9cf746f5
Valerio Guarrasi <31849276+guarrasi1995@users.noreply.github.com>
2018-03-22 10:03:29 +0100

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -20,12 +20,22 @@ from players as p join players_teams as pt join teams as t
 where p.playerID = pt.playerID and pt.tmID = t.tmID and p.firstName="Julius" and p.lastName = "Erving";
 
 # 3
-# 
-
+#The number of choaches awards for the top ten team in descresent order
+select distinct t.name, count(ac.id)
+from coaches as c join awards_coaches as ac join teams as t
+where ac.coachID = c.coachID and ac.year = c.year and c.year = t.year and c.tmID = t.tmID
+group by t.name
+order by count(ac.id) desc
+limit 10;
 
 # 4
-#
+#Top 50 Players of all time
+select p.firstName, p.lastName, sum(pt.points)+ 1.7*sum(pt.rebounds) + 2.2*sum(pt.assists) + 6.1*sum(pt.blocks) + 9.1*sum(pt.steals) + 5.4*sum(pt.turnovers) as score
+from players_teams as pt join players as p
+where pt.playerID = p.playerID
+group by pt.playerID
+order by sum(pt.points)+ 1.7*sum(pt.rebounds) + 2.2*sum(pt.assists) + 6.1*sum(pt.blocks) + 9.1*sum(pt.steals) + 5.4*sum(pt.turnovers) desc
+limit 50;
 
 #5
 #
-select lastName from players;
